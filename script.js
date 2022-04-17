2bdffd52844592791c2bba11945f7713c62b32ad
Alexsandro Gomes Clemente <alexsandro.gomes.clemente@hotmail.com>
2022-04-17 11:49:27 -0300

    Projeto Relogio

---
+++ script.js
@@ -0,0 +1,36 @@
+/* Variaveis */
+let digitalElement = document.querySelector(".digital")
+let sElement = document.querySelector(".p_s")
+let mElement = document.querySelector(".p_m")
+let hElement = document.querySelector(".p_h")
+
+
+/*Functions */
+
+const updateClock = () => {
+    let now = new Date();
+    let hour = now.getHours()
+    let minute = now.getMinutes()
+    let second = now.getSeconds()
+
+    let sDeg = ((360/60) * second ) - 90
+    let mDeg = ((360/60) * minute ) - 90
+    let hDeg = ((360/12) * hour ) - 90
+
+    digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}` // Relogio Digital 
+
+    sElement.style.transform =`rotate(${sDeg}deg)`
+    mElement.style.transform =`rotate(${mDeg}deg)`
+    hElement.style.transform =`rotate(${hDeg}deg)`
+
+
+}
+
+
+
+const fixZero = (time) =>  time < 10 ? '0'+ time : time  // Function para adicionar um zero ao relogio 
+   
+
+
+setInterval(updateClock,1000)
+updateClock()
